184762df6d8083e8bf6fd62fb839db90f6b7e5fb
Athul Kesav <me.nd.mself@gmail.com>
2025-05-03 16:17:25 +0530

    Another config change

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  output: 'export',
-  basePath: '/brahmma', // Replace with your actual repo name
-  assetPrefix: '/brahmma',
-
   
 };
 
