c0ce8a75ee034fe8288059829de8cdab1a3b78f7
Athul Kesav <me.nd.mself@gmail.com>
2025-05-03 15:52:48 +0530

    Deployment setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: 'export',
+  basePath: '/brahmma', // Replace with your actual repo name
+  assetPrefix: '/brahmma',
+
   
 };
 
