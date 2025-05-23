5ac47de6319af811f5be025c361ecaf1cf9bcc19
Athul Kesav <me.nd.mself@gmail.com>
2025-05-23 22:43:09 +0530

    removed config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  basePath: '/brahmma', // if you host under /<repo-name>
-  assetPrefix: '/brahmma',
+
 };
 
 export default nextConfig;
